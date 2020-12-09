921042c6c12e697aa2a992269c803180cd7d5d20
vinid <chiccobia@gmail.com>
2020-12-09 22:57:45 +0100

    new tests

--- tests/test_typing_model.py
+++ tests/test_typing_model.py
@@ -9,10 +9,9 @@ from typing_model.data.parse_dataset import DatasetParser
 class TestTyping_model(unittest.TestCase):
     """Tests for `typing_model` package."""
 
-
     def test_datasets(self):
         pt = DatasetParser("examples/toy_dataset.json")
-        parsed = pt.parse_dataset()
-        assert len(parsed[0]) == 2
+        id2label, label2id, vocab_len = pt.collect_global_config()
+        mention_train, left_side_train, right_side_train, label_train = pt.parse_dataset()
+        assert len(mention_train) == 2
 
-        TypingBERTDataSet(*parsed)
